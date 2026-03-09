bd766713a544ff7179bc9926085358c85c0cc25f
Via Agent <via@e2b.dev>
2026-03-09 19:43:14 +0000

    feat: add Connect 4 game with AI opponent

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Home from './pages/Home.jsx';
+import ConnectFour from './pages/ConnectFour.jsx';
+
+function App() {
+  return (
+    <Router>
+      <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Via Web App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/connect4">Connect 4</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/connect4" element={<ConnectFour />} />
+        </Routes>
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
